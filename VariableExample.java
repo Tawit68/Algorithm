f8758eb3efe8046facf3aefa2be2a7088a906c02
Tawit68 <684259011@webmail.npru.ac.th>
2025-07-25 12:02:58 +0700

    first commit

---
+++ VariableExample.java
@@ -0,0 +1,18 @@
+public class VariblesExample {
+    public static void main(static[] args) {
+        char clothingSize = 's'; 
+        int websiteVisitors = 0;
+        char studentGrade = 'A',;
+        double StudentGPA = 4.0;
+        final double PI = 3.14159;
+        float annualExpenses = 15000.00f;
+        final int MONTHS_IM_YEAR = 12;
+        System.out.println("clothing Size: "+clothingSize);
+        System.out.println("Website Visitors: "+websiteVisitors);
+        System.out.println("Student Grade: "+studentGrade);
+        System.out.println("student GPA: "+StudentGPA)
+        System.out.println("Value of Pi:"+PI);
+        System.out.println("Annual Expenses:"+annualExpenses);
+        System.out.println("Months in Year: "+MONTHS_IM_YEAR );
+    }
+}
